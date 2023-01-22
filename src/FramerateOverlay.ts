226613fcccd03e56c1feebb07fa153aab21615a7
Aaron Faanes <dafrito@gmail.com>
2023-01-22 06:16:37 -0800

    Split out FramerateOverlay

---
+++ src/FramerateOverlay.ts
@@ -0,0 +1,107 @@
+import { Projector } from "parsegraph-projector";
+
+export class TimeRange<T = any> {
+  _startTime: number;
+  _finishTime: number;
+  _val: T;
+
+  constructor(val?: T) {
+    this._startTime = Date.now();
+    this._finishTime = NaN;
+    this._val = val;
+  }
+
+  value() {
+    return this._val;
+  }
+
+  finish() {
+    this._finishTime = Date.now();
+  }
+
+  duration() {
+    return this._finishTime - this._startTime;
+  }
+}
+
+export default class FramerateOverlay {
+  _frames: TimeRange[];
+  _currentPaint: TimeRange;
+  _currentRender: TimeRange;
+
+  constructor() {
+    this._frames = [];
+    this._currentRender = null;
+    this._currentPaint = null;
+  }
+
+  invalidated() {}
+
+  startPaint() {
+    this._currentPaint = new TimeRange("paint");
+    this._frames.push(this._currentPaint);
+  }
+
+  finishPaint() {
+    this._currentPaint?.finish();
+  }
+
+  startRender() {
+    this._currentRender = new TimeRange("render");
+    this._frames.push(this._currentRender);
+  }
+
+  finishRender() {
+    this._currentRender?.finish();
+  }
+
+  draw(proj: Projector) {
+    while (this._frames.length > proj.width()) {
+      this._frames.shift();
+    }
+    const lineHeight = 22;
+    const width = proj.width();
+    const height = proj.height();
+    const ctx = proj.overlay();
+
+    ctx.clearRect(0, height - proj.height() / 3, width, proj.height() / 3);
+    ctx.font = "18px monospace";
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+
+    this._frames.forEach((range, i) => {
+      ctx.fillStyle = range.value() === "paint" ? "green" : "blue";
+      const dur = Math.min(proj.height() / 3, range.duration());
+      ctx.fillRect(i, height - dur, 1, dur);
+    });
+
+    ctx.fillStyle = "blue";
+    ctx.textBaseline = "bottom";
+    ctx.textAlign = "right";
+    const renderDuration = this._currentRender?.duration();
+    let line = `Render: ${renderDuration}ms`;
+    ctx.strokeText(line, width, height);
+    ctx.fillText(line, width, height);
+
+    ctx.fillStyle = "green";
+    const paintDuration = this._currentPaint?.duration();
+    line = `Paint: ${paintDuration}ms`;
+    ctx.strokeText(line, width, height - lineHeight);
+    ctx.fillText(line, width, height - lineHeight);
+
+    ctx.fillStyle = "gray";
+    ctx.textBaseline = "bottom";
+    ctx.textAlign = "left";
+    const d = Date.now();
+    ctx.strokeText(d.toString(), 0, height);
+    ctx.fillText(d.toString(), 0, height);
+
+    ctx.fillStyle = "white";
+    ctx.textBaseline = "top";
+    ctx.textAlign = "right";
+    line = `${width}x${height}`;
+    ctx.strokeText(line, width, 0);
+    ctx.fillText(line, width, 0);
+  }
+}
+
--- src/GLWidget.ts
+++ src/GLWidget.ts
@@ -1,6 +1,5 @@
 // TODO Blocks in foreground are rendered
 // improperly relative to the projection matrix.
-import { elapsed } from "parsegraph-timing";
 
 import Color from "parsegraph-color";
 import Block from "./Block";
@@ -22,112 +21,9 @@ import AlphaInput from "./Input";
 import { Renderable } from "parsegraph-timingbelt";
 import Method from "parsegraph-method";
 
+import FramerateOverlay from './FramerateOverlay';
+
 // TODO Mouse input appears to be... strangely interpreted.
-
-class TimeRange<T = any> {
-  _startTime: number;
-  _finishTime: number;
-  _val: T;
-
-  constructor(val?: T) {
-    this._startTime = Date.now();
-    this._finishTime = NaN;
-    this._val = val;
-  }
-
-  value() {
-    return this._val;
-  }
-
-  finish() {
-    this._finishTime = Date.now();
-  }
-
-  duration() {
-    return this._finishTime - this._startTime;
-  }
-}
-
-class FramerateOverlay {
-  _frames: TimeRange[];
-  _currentPaint: TimeRange;
-  _currentRender: TimeRange;
-
-  constructor() {
-    this._frames = [];
-    this._currentRender = null;
-    this._currentPaint = null;
-  }
-
-  invalidated() {}
-
-  startPaint() {
-    this._currentPaint = new TimeRange("paint");
-    this._frames.push(this._currentPaint);
-  }
-
-  finishPaint() {
-    this._currentPaint?.finish();
-  }
-
-  startRender() {
-    this._currentRender = new TimeRange("render");
-    this._frames.push(this._currentRender);
-  }
-
-  finishRender() {
-    this._currentRender?.finish();
-  }
-
-  draw(proj: Projector) {
-    while (this._frames.length > proj.width()) {
-      this._frames.shift();
-    }
-    const lineHeight = 22;
-    const width = proj.width();
-    const height = proj.height();
-    const ctx = proj.overlay();
-
-    ctx.clearRect(0, height - proj.height() / 3, width, proj.height() / 3);
-    ctx.font = "18px monospace";
-    ctx.fillStyle = "white";
-    ctx.strokeStyle = "black";
-
-    this._frames.forEach((range, i) => {
-      ctx.fillStyle = range.value() === "paint" ? "green" : "blue";
-      const dur = Math.min(proj.height() / 3, range.duration());
-      ctx.fillRect(i, height - dur, 1, dur);
-    });
-
-    ctx.fillStyle = "blue";
-    ctx.textBaseline = "bottom";
-    ctx.textAlign = "right";
-    const renderDuration = this._currentRender?.duration();
-    let line = `Render: ${renderDuration}ms`;
-    ctx.strokeText(line, width, height);
-    ctx.fillText(line, width, height);
-
-    ctx.fillStyle = "green";
-    const paintDuration = this._currentPaint?.duration();
-    line = `Paint: ${paintDuration}ms`;
-    ctx.strokeText(line, width, height - lineHeight);
-    ctx.fillText(line, width, height - lineHeight);
-
-    ctx.fillStyle = "gray";
-    ctx.textBaseline = "bottom";
-    ctx.textAlign = "left";
-    const d = Date.now();
-    ctx.strokeText(d.toString(), 0, height);
-    ctx.fillText(d.toString(), 0, height);
-
-    ctx.fillStyle = "white";
-    ctx.textBaseline = "top";
-    ctx.textAlign = "right";
-    line = `${width}x${height}`;
-    ctx.strokeText(line, width, 0);
-    ctx.fillText(line, width, 0);
-  }
-}
 
 // test version 1.0
 export default class AlphaGLWidget implements Renderable {
